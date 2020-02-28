4e30e4a322ca0ea4d8c79d1050bab26725f9f486
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-28 17:22:58 +0300

    Simple chat

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+const server = require("http").Server(app)
+//create a port whith socket io
+const io = require("socket.io")(server)
+
+app.set("views", "./views")
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+
+const rooms = {}
+
+app.get("/", (req, res) => {
+  res.render("index", { rooms: rooms})
+})
+
+app.get("/:room", (res, req) => {
+  res.render("room", {roomName: req.params.room})
+})
+
+server.listen(3000)
+
+const users = {}
+//this will run every time user connect cite
+io.on("connection", socket => {
+  socket.on("new-user", name => {
+    users[socket.id] = name
+    socket.broadcast.emit("user-connected", name)
+  })
+  socket.on("send-chat-message", message => {
+    socket.broadcast.emit("chat-message", {
+      message,
+      name: users[socket.id]
+    })
+  })
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id])
+    delete users[socket.id]
+  })
+})
