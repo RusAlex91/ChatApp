f460be6690402c02a58f161d94efa4121ba374e8
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-29 14:05:15 +0300

    Socket try heroku

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
-const port = process.env.PORT
-const socket = io(port)
+const socket = io("http://localhost:3000")
 const messageForm = document.getElementById("send-container")
 const messageInput = document.getElementById("message-input")
 const messageContainer = document.getElementById("message-container")
